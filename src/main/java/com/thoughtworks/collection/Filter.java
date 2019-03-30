2e7af09ad954702b9ff56b31ce77c5bf1ed94684
kikychn <kikychn@gmail.com>
2019-03-30 20:29:39 +0800

    update the function of getDifferentElements()

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -26,11 +26,11 @@ public class Filter {
         return array.stream().filter(number -> number % 3 == 0).collect(Collectors.toList());
     }
 
-    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+    public List<Integer> getDifferentElements() {
+        return array.stream().distinct().collect(Collectors.toList());
     }
 
-    public List<Integer> getDifferentElements() {
+    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
         throw new NotImplementedException();
     }
 }
