7bb5175951cafd5f1093e2980867882d1c09518f
kikychn <kikychn@gmail.com>
2019-03-30 21:13:22 +0800

    update the function of transformToOneDimesional()

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,17 +2,24 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        List<Integer> result = new ArrayList<>();
+        for (Integer[] numberList : array) {
+            result.addAll(Arrays.asList(numberList));
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
