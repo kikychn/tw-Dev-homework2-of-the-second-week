83733d727f80f85bc05230f5df98cf4f0db20940
kikychn <kikychn@gmail.com>
2019-03-30 20:19:13 +0800

    update the function of filterEven()

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -4,17 +4,22 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    List<Integer>  array;
+    List<Integer> array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
+    }
+
+    private boolean isEven(int i) {
+        return i % 2 == 0;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream().filter(number -> isEven(number)).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
